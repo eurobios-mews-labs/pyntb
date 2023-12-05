54fc62031cde4751a254b5bd674bad9eb527e174
ecieren <ecieren@eurobios.com>
2023-12-05 12:49:54 +0100

    tests

---
+++ test/test_optimize.py
@@ -0,0 +1,73 @@
+# Copyright 2023 Eurobios Mews Labs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Misc. numeric functions (test)."""
+
+import numpy as np
+
+from pydnt.optimize import bisect_v, fixed_point, qnewt2d_v
+
+
+def test_bisect():
+    size = 99
+    tol = 1.0E-09
+
+    c = np.linspace(1, size, size) + np.random.randn(size)
+    c = np.abs(c)
+
+    def fun(x):
+        return x**2 - c
+
+    r0 = np.sqrt(c)
+
+    x0, err = bisect_v(fun, 0., np.sqrt(np.max(c)) * 1.1, (size,), print_err=True,
+                       tol=tol, maxiter=99)
+
+    assert np.max(np.abs(x0 - r0) <= np.minimum(tol, err))
+
+
+def test_fixed_point():
+    size = 99
+    tol = 1.0E-09
+
+    c = 1 + np.sqrt(np.linspace(1, 10, size))
+
+    def fun(x):
+        return np.sin(c * x)
+
+    x0 = fixed_point(fun, 0.5, xtol=tol, maxiter=999)
+
+    assert np.max(np.abs(x0 - fun(x0))) < tol
+
+
+def test_qnewt2d():
+    size = 99
+    tol = 1.0E-12
+
+    a = np.abs(1 + np.random.randn(size))
+    b = np.abs(1 + np.random.randn(size))
+
+    def f1(x, y):
+        return y - a * x**2
+
+    def f2(x, y):
+        return y - b * x**3
+
+    xg = np.ones((size,))
+    yg = np.ones((size,))
+    x, y, count, err = qnewt2d_v(f1, f2, xg, yg, rtol=tol, maxiter=999, dx=1.0E-09, dy=1.0E-09)
+
+    assert np.logical_and(np.max(np.abs(f1(x, y))) < tol,
+                          np.max(np.abs(f2(x, y))) < tol)
---
+++ test/test_polynomial.py
@@ -0,0 +1,121 @@
+# Copyright 2023 Eurobios Mews Labs
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""Analytical root finding with array inputs (test)."""
+
+import numpy as np
+import pytest
+
+from pydnt.polynomial import solve_p2_v, _cardan_v, solve_p3_v
+
+
+def _maxabs(x):
+    return np.max(np.abs(x))
+
+
+@pytest.fixture
+def sizetol():
+    return 99, 1.0E-10
+
+
+def test_solve_p2_distinct_real_roots(sizetol):
+    size, tol = sizetol
+
+    r1 = np.random.randn(size)
+    r2 = np.random.randn(size)
+
+    def fun(x):
+        return (x - r1) * (x - r2)
+
+    x1, x2 = solve_p2_v(1., -(r1 + r2), r1 * r2)
+
+    assert np.logical_and.reduce((_maxabs(fun(x1)) < tol,
+                                  _maxabs(fun(x2)) < tol,
+                                  np.max(np.minimum(np.abs(r1 - x1),
+                                                    np.abs(r1 - x2))) < tol,
+                                  np.max(np.minimum(np.abs(r2 - x1),
+                                                    np.abs(r2 - x2))) < tol,
+                                  ))
+
+
+def test_solve_p2_double_real_root(sizetol):
+    size, tol = sizetol
+
+    rd = np.random.randn(size)
+
+    def fun(x):
+        return (x - rd)**2
+
+    x1, x2 = solve_p2_v(1., -2 * rd, rd**2)
+
+    assert np.logical_and.reduce((_maxabs(fun(x1)) < tol,
+                                  _maxabs(fun(x2)) < tol,
+                                  _maxabs(x1 - rd) < tol,
+                                  _maxabs(x2 - rd) < tol,
+                                  ))
+
+
+def test_solve_p2_complex_roots(sizetol):
+    size, tol = sizetol
+
+    r1 = np.random.randn(size) + 1j * np.random.randn(size)
+    r2 = np.random.randn(size) + 1j * np.random.randn(size)
+
+    def fun(x):
+        return (x - r1) * (x - r2)
+
+    x1, x2 = solve_p2_v(1., -(r1 + r2), r1 * r2)
+
+    assert np.logical_and.reduce((_maxabs(fun(x1)) < tol,
+                                  _maxabs(fun(x2)) < tol,
+                                  np.max(np.minimum(np.abs(r1 - x1),
+                                                    np.abs(r1 - x2))) < tol
+                                  ))
+
+
+def test_solve_cardan(sizetol):
+    size, tol = sizetol
+
+    p = np.random.randn(size)
+    q = np.random.randn(size)
+
+    def fun(x):
+        return x**3 + p * x + q
+
+    x1, x2, x3 = _cardan_v(p, q)
+
+    assert np.logical_and.reduce((_maxabs(fun(x1)) < tol,
+                                  _maxabs(fun(x2)) < tol,
+                                  _maxabs(fun(x3)) < tol,
+                                  ))
+
+
+def test_solve_p3(sizetol):
+    size, tol = sizetol
+
+    a = np.random.randn(size)
+    b = np.random.randn(size)
+    c = np.random.randn(size)
+    d = np.random.randn(size)
+
+    def fun(x):
+        return a * x**3 + b * x**2 + c * x + d
+
+    x1, x2, x3 = solve_p3_v(a, b, c, d)
+
+    assert np.logical_and.reduce((_maxabs(fun(x1)) < tol,
+                                  _maxabs(fun(x2)) < tol,
+                                  _maxabs(fun(x3)) < tol,
+                                  ))
